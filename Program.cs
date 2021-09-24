b9ef8026efa86c6c7f93dfce7a4aee935310642d
natandovale <natandovale496@gmail.com>
2021-09-24 16:44:44 -0300

    Exemplo codigo usando interface IComparable

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list){
+                        Console.WriteLine(emp);
+                    }   
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
